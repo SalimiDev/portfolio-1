f6a34f25b91e0f1be9bf70cb30966940e1383fda
Mehdi Salimi <Salimi.devop@gmail.com>
2022-12-29 18:20:09 +0330

    build:added folder structures of project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+// components
+import { Navbar } from './components';
+//container
+import { About, Footer, Header, Skills, Testimonial, Work, work } from './container';
+
 const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  );
+    return (
+        <div className='app'>
+            <Navbar />
+            <Header />
+            <About />
+            <Work />
+            <Skills />
+            <Testimonial />
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
