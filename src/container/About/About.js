1ac1f765850c24f9c4152dfc6b0ff34c3f9a561a
Mehdi Salimi <Salimi.devop@gmail.com>
2023-01-08 02:11:58 +0330

    build:setup sanity and connect to the client

--- src/container/About/About.js
+++ src/container/About/About.js
@@ -3,15 +3,17 @@ import './About.scss';
 
 import { motion } from 'framer-motion';
 import { images } from '../../constants';
-
-const abouts = [
-    { title: 'Web Development', description: 'I am a good web developer', imgUrl: images.about01 },
-    { title: 'Frontend Development', description: 'I am a good web developer', imgUrl: images.about02 },
-    { title: 'React Js', description: 'I am a good web developer', imgUrl: images.about03 },
-    { title: 'Next.Js', description: 'I am a good web developer', imgUrl: images.about04 },
-];
+import { client, urlFor } from '../../client';
 
 const About = () => {
+    const [abouts, setAbouts] = useState([]);
+
+    useEffect(() => {
+        const query = '*[_type=="abouts"]';
+
+        client.fetch(query).then(data => setAbouts(data));
+    }, []);
+
     return (
         <>
             <h2 className='head-text'>
@@ -26,7 +28,7 @@ const About = () => {
                         transition={{ duration: 0.5, type: 'tween' }}
                         className='app__profile-item'
                         key={about.title + index}>
-                        <img src={about.imgUrl} alt={about.title} />
+                        <img src={urlFor(about.imgUrl)} alt={about.title} />
                         <h2 className='bold-text' style={{ marginTop: 20 }}>
                             {about.title}
                         </h2>
